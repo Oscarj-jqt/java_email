f5381e40694f6bd615ac345eee443fd456fc9d10
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-06-03 22:52:46 +0200

    final commit

--- src/src/emailapp/EmailApp.java
+++ src/src/emailapp/EmailApp.java
@@ -4,7 +4,7 @@ package emailapp;
 public class EmailApp {
 
     public static void main(String[] args) {
-        //On crée un objet à partir du constructeur créé dans Email
+        //On crée un objet à partir du constructeur créé dans Email.
         Email em1 = new Email("Oscar","Jacquet");
         System.out.println(em1.showInfo());
         // On peut changer le mot de passe, altérer l'email avec les méthodes créées
